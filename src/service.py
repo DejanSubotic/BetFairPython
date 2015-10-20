f645c93886dc0f4a9fbf7cd2d04916a38d856ded
Dejan Subotic <dejan.subotic@hrmsoftware.se>
2015-10-20 13:11:49 +0200

    Intial config

--- src/service.py
+++ src/service.py
@@ -1,14 +1,14 @@
 import requests
 import json
- 
+
 endpoint = "https://api.betfair.com/exchange/betting/rest/v1.0/"
  
-header = { 'X-Application' : 'JOIdNhZjPP4AK9F4', 'X-Authentication' : 'o5458ZOim8wTO2ety08JS9cnc1bYHtf0+Pp/bk5+sXo=' ,'content-type' : 'application/json' }
+header = { 'X-Application' : 'JOIdNhZjPP4AK9F4', 'X-Authentication' : 'snbcJvB8HBRDrjoXPgrdCh+n7njJDx3AKD106Hk5cjg=' ,'content-type' : 'application/json' }
  
 json_req='{"filter":{ }}'
  
 url = endpoint + "listEventTypes/"
- 
+
 response = requests.post(url, data=json_req, headers=header)
  
  
